19dcd3bac95f426251228fb1b9994e7915abd37c
Simon Debus <simon.debus@iu-study.org>
2025-04-10 18:35:36 +0200

    Added english components + Header and footer components are now loaded in english or german depending on the page language. Index.html translation.

--- loader.js
+++ loader.js
@@ -1,9 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("components/header.html")  
-        .then(response => response.text())  
-        .then(data => document.querySelector("header").innerHTML = data);  
-
-    fetch("components/footer.html") 
-        .then(response => response.text())  
-        .then(data => document.querySelector("footer").innerHTML = data);  
-});
+    const isEnglish = window.location.pathname.includes("-en.html");
+  
+    const headerFile = isEnglish ? "components/header-en.html" : "components/header.html";
+    const footerFile = isEnglish ? "components/footer-en.html" : "components/footer.html";
+  
+    fetch(headerFile)
+      .then(response => response.text())
+      .then(data => document.querySelector("header").innerHTML = data);
+  
+    fetch(footerFile)
+      .then(response => response.text())
+      .then(data => document.querySelector("footer").innerHTML = data);
+  });
+  
